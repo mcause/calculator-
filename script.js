0784c02e3db06c659ad7b38e8fea2f80c061497d
mcause <m.cause88@gmail.com>
2021-12-29 18:30:37 -0500

    updated the minus button and add more css

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // create the calculator boarder 
-// create buttons for different functions 
-// make the functions work once clicked on 
-// make the result of the function appear on the screen 
-// make a clear button that wont accept the previous inputs
+// create buttons for different inputs 
+// make the buttons show the input once clicked 
+// make the result of 2 inputs appear on the screen 
+// create a clear button that will erase the data that was input 
+// create a equal button that will output the whatever was clicked 
+// create a funtion for solving the problem created
 // make the calculator enjoyable but make it work !!!
 
 const calculator = document.querySelector('.calculator');
@@ -30,7 +32,6 @@ function clearInput(){
 }
 // created and event once clicked will clear the calculator screen
 clearButton. addEventListener('click', clearInput)
-
 //created a similar function for the equal sign 
 function equalInput(){
   //set the output text to equal the solving function and return the answer in on the calculator screen
@@ -39,38 +40,26 @@ function equalInput(){
 //Added an event once equal sign is clicked it will display the answer of the math problem 
 equalSign.addEventListener('click', equalInput)
 
-//Creat a funtion that will output the ansewer of the chosen numbers 
+//Creat a funtion that will output the ansewer of the chosen numbers using the
 function solving(numInput){
-//creatred a for loop to look to constantly look for one of the following inputs
+//created a for loop to constantly look for one of the following inputs
 for(let i = 0; i < numInput.length; i++){
   //created an if statement for the first condition 
   if(numInput[i] === '+'){
-    //create a variable that turns the bumbers into a string and splits it with the + sign 
+    //create a variable that turns the numbers into a string and splits it with the + sign 
     let add = numInput.split('+')
-    // return the first number(add[0]) and by using parseInt turns the string back to a number 
+    // return the first integer(add[0]) plus the second integer
     return(parseInt(add[0])+parseInt(add[1]))
     }else if(numInput[i] === '-'){
-      let minus = minus.split('-')
-      return(parseInt(minus[0])-parseInt(minus[1]))
+      let subtract = numInput.split('-')
+      return(parseInt(subtract[0])-parseInt(subtract[1])) 
     }else if(numInput[i] === '/'){
       let divide = numInput.split('/')
       return(parseInt(divide[0])/parseInt(divide[1]))
-    }else if(numInput[i] === '*'){
-      let add = numInput.split('*')
+    }else if (numInput[i] === '*'){
+      let multiply = numInput.split('*')
       return(parseInt(multiply[0])*parseInt(multiply[1]))
     }
   }
 }
 
-
-
-// make an equal sign 
-// once clicked it returns the sum of the math problem
-  // if clicked again it add the last number to that sum 
-
-
-// make a clear button that clicked 1 time 
-  // clears the previous input 
-  // if clicked twice it clears all of the data
-
-  
